e98623952556af6fb5e7205b94924195807cf9af
cyclops <marcgrosz+github@icloud.com>
2016-01-20 00:09:05 -0500

    chore: readying for pypi

--- StopWatch/__init__.py
+++ StopWatch/__init__.py
@@ -2,3 +2,5 @@
 """
 StopWatch Package
 """
+__author__ = 'Marc Grosz <cyclops3590@gmail.com>'
+__version__ = '0.3.0'
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='StopWatch',
+        version='0.3.0',
+        packages=['tests', 'StopWatch'],
+        url='https://github.com/cyclops3590/StopWatch',
+        license='None',
+        author='cyclops',
+        author_email='cyclops3590@gmail.com',
+        description='Multiclock Stopwatch'
+)
